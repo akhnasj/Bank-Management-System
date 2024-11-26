57f230165492cc6d4f0b5dd52d4a72b779915605
Akhna <akhnasj1612@gmail.com>
2024-11-27 02:14:11 +0530

    Added Connection

--- src/BMS/Conn.java
+++ src/BMS/Conn.java
@@ -6,6 +6,7 @@ import java.sql.*;
 public class Conn {
     Connection connection;
     Statement statement;
+    public Object s;
     public Conn() {
         try{
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank", "root", "toor");
