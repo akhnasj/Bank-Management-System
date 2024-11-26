46f2e2de22ae4ec4381cb56496da243dc48d8e4c
Akhna <akhnasj1612@gmail.com>
2024-11-26 18:30:09 +0530

    Modified MiniStatement

--- src/BMS/MiniStatement.java
+++ src/BMS/MiniStatement.java
@@ -1,3 +1,5 @@
+package BMS;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -13,9 +15,8 @@ public class MiniStatement extends JFrame implements ActionListener{
     JButton b1;
 
     MiniStatement(String pinno){
-
-
-            this.pinno=pinno;
+    
+        this.pinno = pinno;
 
         setSize(400,600);
         setLayout(null);
@@ -23,82 +24,72 @@ public class MiniStatement extends JFrame implements ActionListener{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       
 
-       JLabel label1= new JLabel();
-       label1.setBounds(20,140,400,20);
-       add(label1);
+        JLabel label1 = new JLabel();
+        label1.setBounds(20,140,400,20);
+        add(label1);
 
-       JLabel label2 = new JLabel();
-       label2.setBounds(150,20,200,20);
-       label2.setFont(new Font("System",Font.BOLD,15));
+        JLabel label2 = new JLabel();
+        label2.setBounds(150,20,200,20);
+        label2.setFont(new Font("System",Font.BOLD,15));
         add(label2);
 
         JLabel label3 = new JLabel();
         label3.setBounds(150,80,300,20);
         label3.setFont(new Font("System",Font.BOLD,15));
-         add(label3);
+        add(label3);
 
-         JLabel label4 = new JLabel();
+        JLabel label4 = new JLabel();
         label4.setBounds(20,400,300,20);
         label4.setFont(new Font("System",Font.BOLD,15));
-         add(label4);
+        add(label4);
 
-
-
-         try{
-
-            connection c = new connection();
+        try{ 
+            Conn c = new Conn();
             ResultSet rs = c.statement.executeQuery("select * from login where pinno='"+pinno+"'");
             while(rs.next()){
                 label3.setText("CardNumber: "+rs.getString("cardno".substring(0,4) +"XXXXXXXXX"+rs.getString("cardno").substring(12)));
             }
 
-         }catch(Exception E){
+        }catch(Exception E){
 
-         }
+        }
 
-         try{
+        try{
 
-            int balance =0;
-            connection c = new connection();
+            int balance = 0;
+            Conn c = new Conn();
             ResultSet resultSet = c.statement.executeQuery("select * from bank where pinno = '"+pinno+"'");
-           
+            
             while(resultSet.next()){
                 label1.setText(label1.getText() + "<html>" +resultSet.getString("date") +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +resultSet.getString("type")+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+resultSet.getString("amount")+ "<br><br><br></html>");
 
                 if(resultSet.getString("type").equals("Deposit")){
-
                     balance+=Integer.parseInt(resultSet.getString("amount"));
-
                 }else{
                     balance-=Integer.parseInt(resultSet.getString("amount"));
-
                 }
             }
 
             label4.setText("Your Total Balance is Rs. "+balance);
 
-         }catch(Exception e){
+        }catch(Exception e){
             e.printStackTrace();
-         }
+        }
 
-
-         b1 = new JButton("Exit");
-         b1.setBounds(20,500,100,25);
-         add(b1);
-
+        b1 = new JButton("Exit");
+        b1.setBounds(20,500,100,25);
+        add(b1);
 
         setLayout(null);
-       setVisible(true);
-
-            
+        setVisible(true);       
     }
+
     public static void main(String[] args){
-
-      new MiniStatement(" ");
+        new MiniStatement(" ");
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-    setVisible(false);
+        setVisible(false);
     }
 }
