f24432adf27832f114bda8d5af8dd5d5a0f4487f
Akhna <akhnasj1612@gmail.com>
2024-11-27 02:14:40 +0530

    Modified Login

--- src/BMS/Login.java
+++ src/BMS/Login.java
@@ -6,11 +6,11 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
@@ -20,8 +20,7 @@ public class Login extends JFrame implements ActionListener {
     JPasswordField passwordField3;
     JButton button1, button2, button3;
 
-
-    Login(){
+    Login() {
         super("Bank Management System");
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/bank.png"));
@@ -38,26 +37,22 @@ public class Login extends JFrame implements ActionListener {
         iimage.setBounds(630, 350, 100, 100);
         add(iimage);
 
-
-
         label1 = new JLabel("WELCOME TO ATM");
         label1.setForeground(Color.DARK_GRAY);
         label1.setFont(new Font("AvantGarde", Font.BOLD, 38));
         label1.setBounds(230, 125, 450, 40);
         add(label1);
 
-
         label2 = new JLabel("Card Number: ");
         label2.setForeground(Color.DARK_GRAY);
-        label2.setFont(new Font("Ralway", Font.BOLD,18));
-        label2.setBounds(150, 200,375,30);
+        label2.setFont(new Font("Ralway", Font.BOLD, 18));
+        label2.setBounds(150, 200, 375, 30);
         add(label2);
 
         textField2 = new JTextField(15);
         textField2.setBounds(325, 200, 230, 30);
         textField2.setFont(new Font("Arial", Font.BOLD, 16));
         add(textField2);
-
 
         label3 = new JLabel("PIN: ");
         label3.setForeground(Color.DARK_GRAY);
@@ -70,32 +65,29 @@ public class Login extends JFrame implements ActionListener {
         passwordField3.setFont(new Font("Arial", Font.BOLD, 16));
         add(passwordField3);
 
-
-
         button1 = new JButton("SIGN IN");
         button1.setFont(new Font("Arial", Font.BOLD, 14));
         button1.setForeground(Color.WHITE);
         button1.setBackground(Color.BLACK);
         button1.setBounds(300, 300, 100, 30);
         button1.addActionListener(this);
-        add(button1); 
+        add(button1);
 
-        button2 = new JButton("CLEAR ");
+        button2 = new JButton("CLEAR");
         button2.setFont(new Font("Arial", Font.BOLD, 14));
         button2.setForeground(Color.WHITE);
         button2.setBackground(Color.BLACK);
         button2.setBounds(430, 300, 100, 30);
         button2.addActionListener(this);
-        add(button2); 
+        add(button2);
 
-        button3 = new JButton("SIGN UP ");
+        button3 = new JButton("SIGN UP");
         button3.setFont(new Font("Arial", Font.BOLD, 14));
         button3.setForeground(Color.WHITE);
         button3.setBackground(Color.BLACK);
         button3.setBounds(300, 350, 230, 30);
         button3.addActionListener(this);
         add(button3);
-
 
         ImageIcon iii1 = new ImageIcon(ClassLoader.getSystemResource("icons/bg.jpg"));
         Image iii2 = iii1.getImage().getScaledInstance(850, 480, Image.SCALE_DEFAULT);
@@ -104,39 +96,42 @@ public class Login extends JFrame implements ActionListener {
         iiimage.setBounds(0, 0, 850, 480);
         add(iiimage);
 
-        
         setLayout(null);
         setSize(850, 480);
         setLocation(300, 200);
         setVisible(true);
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        try{
-            if (e.getSource() == button1){
+        try {
+            if (e.getSource() == button1) {
                 Conn c = new Conn();
                 String cardno = textField2.getText();
                 String pin = passwordField3.getText();
-                String q = "select * from login where card_no = '"+cardno+"' and pin = '"+pin+"'";
+                String q = "select * from login where card_no = '" + cardno + "' and pin = '" + pin + "'";
                 ResultSet resultset = c.statement.executeQuery(q);
                 if (resultset.next()) {
                     setVisible(false);
-                    // new main_class(pin);
+                    new Main(pin); // Navigate to Main class with pin
+                } else {
+                    // Show error if credentials are incorrect
+                    JLabel label = new JLabel("Invalid Card Number or PIN");
+                    label.setForeground(Color.RED);
+                    JOptionPane.showMessageDialog(null, "Invalid Credentials");
                 }
-            }else if (e.getSource() == button2){
+            } else if (e.getSource() == button2) {
                 textField2.setText("");
                 passwordField3.setText("");
-
-            }else if (e.getSource() == button3){
+            } else if (e.getSource() == button3) {
                 new Signup1();
                 setVisible(false);
             }
-        }catch (Exception E){
+        } catch (Exception E) {
             E.printStackTrace();
         }
     }
+
     public static void main(String[] args) {
         new Login();
     }
